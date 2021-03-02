04db261175709540479122c25f6c0616612e1e74
danielevergani <dvergani94@gmail.com>
2021-03-02 14:49:24 +0100

    test gerarchia + prime variabili

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert();
+/*
+test per verificare "gerarchia"
+
+var test = 15;
+
+if ((test % 3 == 0) && (test % 5 == 0)){
+    console.log("bella");
+}
+else if(test % 5 == 0){
+    console.log("5");
+}
+else if (test % 3 == 0){
+    console.log("3");
+}
+*/ 
+
+// inizializzo array vuoto + variabili utili
+
+var lista = [];
+var i = 0;
+
+// inizio ciclo
